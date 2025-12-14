e528b6d79ec0d21ad292a02e0d0c115184893a40
Hector Manuel Halty <hectormanuelhalty@gmail.com>
2025-12-14 15:03:44 -0300

    a

--- src/salida.asm
+++ src/salida.asm
@@ -17,13 +17,6 @@ MensajePrintNum db "Salida: %d", 10, 0
 MensajePrintFloat db "Salida: %f", 10, 0
 MaxFloatValue dd 2139095039
 @aux0 dd 0
-@aux1 dd 0
-@aux2 dd 0
-@aux3 dd 0
-@aux4 dd 0
-@aux5 dd 0
-@aux6 dd 0
-@aux7 dd 0
 _RET_VAL_0 dd 0
 _RET_VAL_1 dd 0
 _RET_VAL_2 dd 0
@@ -34,35 +27,15 @@ _RET_VAL_6 dd 0
 _RET_VAL_7 dd 0
 _RET_VAL_8 dd 0
 _RET_VAL_9 dd 0
-_A_PROGRAMA_lambda_1 dd 0
-str_4 db "Imprime Lambda Positivo", 0
-str_6 db "Imprime Lambda Negativo", 0
+_error_tipo dd 0
 .code
 start:
 Label0:
-JMP Label8
+MOV EAX, _error_tipo
+SUB EAX, 1
+JC ErrorRestaNegativa
+MOV @aux0, EAX
 Label1:
-MOV _A_PROGRAMA_lambda_1, ECX
-Label2:
-MOV EAX, _A_PROGRAMA_lambda_1
-CMP EAX, 3
-SETA AL
-MOVZX EAX, AL
-MOV @aux2, EAX
-Label3:
-MOV EAX, @aux2
-CMP EAX, 0
-JE Label6
-Label4:
-invoke crt_printf, addr MensajePrint, addr str_4
-Label5:
-JMP Label7
-Label6:
-invoke crt_printf, addr MensajePrint, addr str_6
-Label7:
-; -- RETURN --
-RET
-Label8:
 invoke ExitProcess, 0
 Error_DivCero:
 invoke crt_printf, addr MsgErrorDivCero
