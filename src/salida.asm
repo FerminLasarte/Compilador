cd8a1bb1ff5a7e6889297ea4c95ce5b543d3dbf9
Fermin Lasarte <fermin.lasarte@icloud.com>
2025-12-15 10:29:49 -0300

    aa

--- src/salida.asm
+++ src/salida.asm
@@ -19,26 +19,6 @@ MaxFloatValue dd 2139095039
 @aux0 dd 0
 @aux1 dd 0
 @aux2 dd 0
-@aux3 dd 0
-@aux4 dd 0
-@aux5 dd 0
-@aux6 dd 0
-@aux7 dd 0
-@aux8 dd 0
-@aux9 dd 0
-@aux10 dd 0
-@aux11 dd 0
-@aux12 dd 0
-@aux13 dd 0
-@aux14 dd 0
-@aux15 dd 0
-@aux16 dd 0
-@aux17 dd 0
-@aux18 dd 0
-@aux19 dd 0
-@aux20 dd 0
-@aux21 dd 0
-@aux22 dd 0
 _RET_VAL_0 dd 0
 _RET_VAL_1 dd 0
 _RET_VAL_2 dd 0
@@ -51,98 +31,18 @@ _RET_VAL_8 dd 0
 _RET_VAL_9 dd 0
 _A_PROGRAMA dd 0
 _B_PROGRAMA dd 0
-str_4 db "A llego a 3UI", 0
-str_9 db "A vale 5UI", 0
-str_14 db "B llego a 5UI", 0
-str_19 db "A: ", 0
-str_21 db "B: ", 0
 .code
 start:
 Label0:
-MOV EAX, 3
+MOV EAX, 65535
 MOV _A_PROGRAMA, EAX
 Label1:
-MOV EAX, 3
-MOV _B_PROGRAMA, EAX
+MOV EAX, -65535
+MOV _A_PROGRAMA, EAX
 Label2:
-MOV EAX, _A_PROGRAMA
-CMP EAX, 3
-SETE AL
-MOVZX EAX, AL
-MOV @aux2, EAX
+MOV EAX, -1
+MOV _B_PROGRAMA, EAX
 Label3:
-MOV EAX, @aux2
-CMP EAX, 0
-JE Label7
-Label4:
-invoke crt_printf, addr MensajePrint, addr str_4
-Label5:
-MOV EAX, _A_PROGRAMA
-ADD EAX, 2
-CMP EAX, 65535
-JA ErrorOverflow
-MOV @aux5, EAX
-Label6:
-MOV EAX, @aux5
-MOV _A_PROGRAMA, EAX
-Label7:
-MOV EAX, _A_PROGRAMA
-CMP EAX, 5
-SETE AL
-MOVZX EAX, AL
-MOV @aux7, EAX
-Label8:
-MOV EAX, @aux7
-CMP EAX, 0
-JE Label12
-Label9:
-invoke crt_printf, addr MensajePrint, addr str_9
-Label10:
-MOV EAX, _B_PROGRAMA
-ADD EAX, 2
-CMP EAX, 65535
-JA ErrorOverflow
-MOV @aux10, EAX
-Label11:
-MOV EAX, @aux10
-MOV _B_PROGRAMA, EAX
-Label12:
-MOV EAX, _B_PROGRAMA
-CMP EAX, 5
-SETE AL
-MOVZX EAX, AL
-MOV @aux12, EAX
-Label13:
-MOV EAX, @aux12
-CMP EAX, 0
-JE Label17
-Label14:
-invoke crt_printf, addr MensajePrint, addr str_14
-Label15:
-MOV EAX, 7
-SUB EAX, 4
-JC ErrorRestaNegativa
-MOV @aux15, EAX
-Label16:
-MOV EAX, @aux15
-MOV _B_PROGRAMA, EAX
-Label17:
-MOV EAX, 9
-SUB EAX, 4
-JC ErrorRestaNegativa
-MOV @aux17, EAX
-Label18:
-MOV EAX, @aux17
-MOV _A_PROGRAMA, EAX
-Label19:
-invoke crt_printf, addr MensajePrint, addr str_19
-Label20:
-invoke crt_printf, addr MensajePrintNum, _A_PROGRAMA
-Label21:
-invoke crt_printf, addr MensajePrint, addr str_21
-Label22:
-invoke crt_printf, addr MensajePrintNum, _B_PROGRAMA
-Label23:
 invoke ExitProcess, 0
 Error_DivCero:
 invoke crt_printf, addr MsgErrorDivCero
