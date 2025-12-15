de53adf4bf2fe8fb87177cca81a0cfae2598e1b8
Fermin Lasarte <fermin.lasarte@icloud.com>
2025-12-15 09:11:27 -0300

    check overflow

--- src/salida.asm
+++ src/salida.asm
@@ -22,7 +22,6 @@ MaxFloatValue dd 2139095039
 @aux3 dd 0
 @aux4 dd 0
 @aux5 dd 0
-@aux6 dd 0
 _RET_VAL_0 dd 0
 _RET_VAL_1 dd 0
 _RET_VAL_2 dd 0
@@ -34,35 +33,35 @@ _RET_VAL_7 dd 0
 _RET_VAL_8 dd 0
 _RET_VAL_9 dd 0
 _A_PROGRAMA dd 0
-str_6 db "hola", 0
+_B_PROGRAMA dd 0
+_C_PROGRAMA dd 0
+_D_PROGRAMA dd 0
 .code
 start:
 Label0:
-MOV EAX, 5
+MOV EAX, 65535
 MOV _A_PROGRAMA, EAX
 Label1:
-invoke crt_printf, addr MensajePrintNum, _A_PROGRAMA
+MOV EAX, -65535
+MOV _B_PROGRAMA, EAX
 Label2:
-MOV EAX, _A_PROGRAMA
-SUB EAX, 1
-JC ErrorRestaNegativa
+MOV EAX, 65534
+ADD EAX, 2
+CMP EAX, 65535
+JA ErrorOverflow
 MOV @aux2, EAX
 Label3:
 MOV EAX, @aux2
-MOV _A_PROGRAMA, EAX
+MOV _C_PROGRAMA, EAX
 Label4:
-MOV EAX, _A_PROGRAMA
-CMP EAX, 0
-SETA AL
-MOVZX EAX, AL
+MOV EAX, 65534
+SUB EAX, 2
+JC ErrorRestaNegativa
 MOV @aux4, EAX
 Label5:
 MOV EAX, @aux4
-CMP EAX, 1
-JE Label1
+MOV _D_PROGRAMA, EAX
 Label6:
-invoke crt_printf, addr MensajePrint, addr str_6
-Label7:
 invoke ExitProcess, 0
 Error_DivCero:
 invoke crt_printf, addr MsgErrorDivCero
